eded176ae8f7eab6579f10b2a3006524c7fa949b
r-goto <r-goto@myMac-16.local>
2022-03-28 21:08:41 +0900

    Fixed working v1.0.0

--- attributes/default_site.rb
+++ attributes/default_site.rb
@@ -1,2 +1,2 @@
-# Specify root directory index file. 'index.html' or 'index2.html'
-default['nginx_setup']['root_index'] = 'index.html'
+# Specify root directory index file. 'index1.html' or 'index2.html'
+default['nginx_setup']['root_index'] = 'index1.html'
--- compliance/profiles/validation_test/controls/nginx_test.rb
+++ compliance/profiles/validation_test/controls/nginx_test.rb
@@ -29,5 +29,5 @@ describe command('curl -s localhost') do
 end
 
 describe file('/usr/share/nginx/html/chef-progress-logo.png') do
-  it { should_not exist }
+  it { should exist }
 end
